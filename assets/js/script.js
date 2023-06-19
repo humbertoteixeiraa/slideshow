fb80ac4b62f8fce6b03aad83a4cfa82e2635943e
Humberto Teixeira <humberto.quim@gmail.com>
2023-06-19 17:13:20 -0300

    Updates of the day 06.19.2023

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let totalSlides = document.querySelectorAll('.slider--item').length;
+
+document.querySelector('.slider--width').style.width = `calc(100vw * ${totalSlides})`;
+document.querySelector('.slider--controls').style.height = 
+    `${document.querySelector('.slider').clientHeight}px`;
+
+//FUNCTIONS
+function goPrev() {
+
+}
+
+function goBack() {
+    
+}
